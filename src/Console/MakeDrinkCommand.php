b5f166a9471b294b97c8fbaadc422f40f2ce9947
giancarlo.morales <giancarlo.morales@estudiantat.upc.edu>
2024-03-04 05:58:04 +0100

    First change MakeDrinkCommand OrderService class created and Docker files added

--- src/Console/MakeDrinkCommand.php
+++ src/Console/MakeDrinkCommand.php
@@ -43,64 +43,59 @@ class MakeDrinkCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $orderService = new OrderService();
+        $orderService->makeDrink($input, $output);
+    }
+}
+
+class OrderService
+{
+    public function makeDrink(InputInterface $input, OutputInterface $output)
+    {
         $drinkType = strtolower($input->getArgument('drink-type'));
         if (!in_array($drinkType, ['tea', 'coffee', 'chocolate'])) {
             $output->writeln('The drink type should be tea, coffee or chocolate.');
+            return;
+        }
+
+        $money = $input->getArgument('money');
+        switch ($drinkType) {
+            case 'tea':
+                if ($money < 0.4) {
+                    $output->writeln('The tea costs 0.4.');
+                    return;
+                }
+                break;
+            case 'coffee':
+                if ($money < 0.5) {
+                    $output->writeln('The coffee costs 0.5.');
+                    return;
+                }
+                break;
+            case 'chocolate':
+                if ($money < 0.6) {
+                    $output->writeln('The chocolate costs 0.6.');
+                    return;
+                }
+                break;
+        }
+
+        $sugars = $input->getArgument('sugars');
+        $stick = false;
+        $extraHot = $input->getOption('extra-hot');
+        if ($sugars >= 0 && $sugars <= 2) {
+            $output->write('You have ordered a ' . $drinkType);
+            if ($extraHot) {
+                $output->write(' extra hot');
+            }
+
+            if ($sugars > 0) {
+                $stick = true;
+                $output->write(' with ' . $sugars . ' sugars (stick included)');
+            }
+            $output->writeln('');
         } else {
-            /**
-             * Tea       --> 0.4
-             * Coffee    --> 0.5
-             * Chocolate --> 0.6
-             */
-            $money = $input->getArgument('money');
-            switch ($drinkType) {
-                case 'tea':
-                    if ($money < 0.4) {
-                        $output->writeln('The tea costs 0.4.');
-                        return;
-                    }
-                    break;
-                case 'coffee':
-                    if ($money < 0.5) {
-                        $output->writeln('The coffee costs 0.5.');
-                        return;
-                    }
-                    break;
-                case 'chocolate':
-                    if ($money < 0.6) {
-                        $output->writeln('The chocolate costs 0.6.');
-                        return;
-                    }
-                    break;
-            }
-
-            $sugars = $input->getArgument('sugars');
-            $stick = false;
-            $extraHot = $input->getOption('extra-hot');
-            if ($sugars >= 0 && $sugars <= 2) {
-                $output->write('You have ordered a ' . $drinkType);
-                if ($extraHot) {
-                    $output->write(' extra hot');
-                }
-
-                if ($sugars > 0) {
-                    $stick = true;
-                    $output->write(' with ' . $sugars . ' sugars (stick included)');
-                }
-                $output->writeln('');
-            } else {
-                $output->writeln('The number of sugars should be between 0 and 2.');
-            }
-
-            $pdo = MysqlPdoClient::getPdo();
-
-            $stmt= $pdo->prepare( 'INSERT INTO orders (drink_type, sugars, stick, extra_hot) VALUES (:drink_type, :sugars, :stick, :extra_hot)');
-            $stmt->execute([
-                'drink_type' => $drinkType,
-                'sugars' => $sugars,
-                'stick' => $stick ?: 0,
-                'extra_hot' => $extraHot ?: 0,
-            ]);
+            $output->writeln('The number of sugars should be between 0 and 2.');
         }
     }
 }
