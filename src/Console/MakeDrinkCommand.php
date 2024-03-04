416a384694f65cf154bd8c9c58805ea3d85ee034
giancarlo.morales <giancarlo.morales@estudiantat.upc.edu>
2024-03-04 06:20:47 +0100

    Added error controls and separated SQL insert in other function

--- src/Console/MakeDrinkCommand.php
+++ src/Console/MakeDrinkCommand.php
@@ -43,13 +43,17 @@ class MakeDrinkCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $orderService = new OrderService();
-        
-        $drinkType = $input->getArgument('drink-type');
+
+        $drinkType = strtolower($input->getArgument('drink-type'));
         $money = $input->getArgument('money');
         $sugars = $input->getArgument('sugars');
         $extraHot = $input->getOption('extra-hot');
-        
-        $orderService->placeOrder($output, $drinkType, $money, $sugars, $extraHot);
+
+        try {
+            $orderService->placeOrder($output, $drinkType, $money, $sugars, $extraHot);
+        } catch (\Exception $e) {
+            $output->writeln($e->getMessage());
+        }
     }
 }
 
@@ -58,62 +62,61 @@ class OrderService
     public function placeOrder(OutputInterface $output, $drinkType, $money, $sugars, $extraHot)
     {
         $this->validateInput($output, $drinkType, $money, $sugars);
-        $this->makeDrink($output, $drinkType, $sugars, $extraHot);
-        $this->storeOrder($drinkType, $sugars, $extraHot);
+        $this->processOrder($output, $drinkType, $sugars, $extraHot);
+        $this->saveOrder($drinkType, $sugars, $extraHot);
     }
 
     private function validateInput(OutputInterface $output, $drinkType, $money, $sugars)
     {
         if (!in_array($drinkType, ['tea', 'coffee', 'chocolate'])) {
-            throw new \InvalidArgumentException('The drink type should be tea, coffee or chocolate.');
+            throw new \Exception('The drink type should be tea, coffee or chocolate.');
         }
 
         switch ($drinkType) {
             case 'tea':
                 if ($money < 0.4) {
-                    throw new \InvalidArgumentException('The tea costs 0.4.');
+                    throw new \Exception('The tea costs 0.4.');
                 }
                 break;
             case 'coffee':
                 if ($money < 0.5) {
-                    throw new \InvalidArgumentException('The coffee costs 0.5.');
+                    throw new \Exception('The coffee costs 0.5.');
                 }
                 break;
             case 'chocolate':
                 if ($money < 0.6) {
-                    throw new \InvalidArgumentException('The chocolate costs 0.6.');
+                    throw new \Exception('The chocolate costs 0.6.');
                 }
                 break;
         }
 
         if ($sugars < 0 || $sugars > 2) {
-            throw new \InvalidArgumentException('The number of sugars should be between 0 and 2.');
+            throw new \Exception('The number of sugars should be between 0 and 2.');
         }
     }
 
-    private function makeDrink(OutputInterface $output, $drinkType, $sugars, $extraHot)
+    private function processOrder(OutputInterface $output, $drinkType, $sugars, $extraHot)
     {
-        $output->write("You have ordered a $drinkType");
+        $output->write('You have ordered a ' . $drinkType);
         if ($extraHot) {
             $output->write(' extra hot');
         }
-
         if ($sugars > 0) {
-            $output->write(" with $sugars sugars (stick included)");
+            $output->write(' with ' . $sugars . ' sugars (stick included)');
         }
         $output->writeln('');
+    }
 
+    private function saveOrder($drinkType, $sugars, $extraHot)
+    {
         $pdo = MysqlPdoClient::getPdo();
 
-        $stmt= $pdo->prepare( 'INSERT INTO orders (drink_type, sugars, stick, extra_hot) VALUES (:drink_type, :sugars, :stick, :extra_hot)');
+        $stmt = $pdo->prepare('INSERT INTO orders (drink_type, sugars, stick, extra_hot) VALUES (:drink_type, :sugars, :stick, :extra_hot)');
         $stmt->execute([
             'drink_type' => $drinkType,
             'sugars' => $sugars,
-            'stick' => $stick ?: 0,
-            'extra_hot' => $extraHot ?: 0,
+            'stick' => $sugars > 0 ? 1 : 0,
+            'extra_hot' => $extraHot ? 1 : 0,
         ]);
     }
 }
-
-
-        
